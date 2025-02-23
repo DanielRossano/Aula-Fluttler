f90361ead194259fc34fa211ca54422e4edbf5f3
danielrossano <daniel.rossano@live.com>
2025-02-23 09:31:07 -0300

    Commit Inicial

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'database_helper.dart';
+import 'contato.dart';
+
+void main() {
+  runApp(const ContactApp());
+}
+
+class ContactApp extends StatelessWidget {
+  const ContactApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Lista de Contatos',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const ContactListScreen(),
+    );
+  }
+}
+
+class ContactListScreen extends StatefulWidget {
+  const ContactListScreen({super.key});
+
+  @override
+  State<ContactListScreen> createState() => _ContactListScreenState();
+}
+
+class _ContactListScreenState extends State<ContactListScreen> {
+  final DatabaseHelper dbHelper = DatabaseHelper.instance;
+  List<Contact> contacts = [];
+}
